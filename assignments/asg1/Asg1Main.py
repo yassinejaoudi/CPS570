3f91fb2185cfe6b0bb648181a17325f841d35188
Yassine Jaoudi <jaoudiyassine@gmail.com>
2020-09-01 12:23:52 -0400

    Added customized URL parser

--- assignments/asg1/Asg1Main.py
+++ assignments/asg1/Asg1Main.py
@@ -7,10 +7,10 @@ Team Members:
 # import libraries or classes
 import validators # source: https://validators.readthedocs.io/en/latest/#
 import sys
-from urllib.parse import urlparse # source: https://docs.python.org/3/library/urllib.parse.html 
-from queue import Queue
+# from urllib.parse import urlparse # source: https://docs.python.org/3/library/urllib.parse.html 
 from Asg1Socket import TCPsocket
 from Asg1Request import Request
+from Asg1Urlparser import URLparser
 
 # TODO: main must accept two args:
     # 1-) number of threads
@@ -21,46 +21,31 @@ def main(): # function, method are the same
 
     mysocket = TCPsocket() # create an object of TCP socket
     mysocket.createSocket()
-    Q = Queue()
     myrequest = Request()
+    myparser  = URLparser()
     
-    # TODO: (Not needed in part 1) Loop over the input file to read URLs 
-    # Not sure if we need to create a socket for each one
-    # try:
-    #     with open("URL-input-100.txt") as file:
-    #         for line in file:
-    #             Q.put(line)
-    # except IOError:
-    #     print('No such file. Please include an existing file name ...')
-    #     exit(1)
-
-    # print("arg: {} & type: {}".format(sys.argv[1],type(str(sys.argv[1]))))
-    # TODO: (Part1) We might need to pass only one arg instead of a list of args to
-    # satisfy part 1 requirement (program must accept a single command-line arg)
-    parsedHost = urlparse(sys.argv[1])
-    host = parsedHost.geturl()
-    if (validators.url(host)):
-        print('URL: {}'.format(host))
-        print('         Parsing URL... host {}, port {}, request /{}'.format(parsedHost.netloc,parsedHost.port,parsedHost.query))
-        getIpInfo = mysocket.getIP(parsedHost.netloc)
-        myIp = getIpInfo[0]
-        print('         Doing DNS... done in {} ms, found {}'.format(round(getIpInfo[1],2), myIp))
-        # TODO: For politeness, the code will need to hit only unique IPs (Check if the ip is unique)
-        # TODO: Abort all pages that takes longer than 10 secs or are more than 2MB
-        port = 80
-        mysocket.connect(myIp, port)
-
-        # build our request
-        msg = myrequest.headRequest(parsedHost.netloc)
-
-        # send out request
-        mysocket.send(msg)
-        data = mysocket.receive() # receive a reply from the server
-        print("data received: ", data)
-        
-        mysocket.close()
-    else:
-        print('Please include a valid URL in command line ...')   
+    URL = sys.argv[1]
+    host, port, path, query  = myparser.parse(URL)
+    
+    # if (validators.url(host)):
+    print('URL: {}'.format(URL))
+    print('         Parsing URL... host {}, port {}, path {}, request {}'.format(host, port, path, query))
+    getIpInfo = mysocket.getIP(host)
+    myIp = getIpInfo[0]
+    print('         Doing DNS... done in {} ms, found {}'.format(round(getIpInfo[1],2), myIp))
+    # TODO: For politeness, the code will need to hit only unique IPs (Check if the ip is unique)
+    # TODO: Abort all pages that takes longer than 10 secs or are more than 2MB
+    mysocket.connect(myIp, port)
+    # build our request
+    msg = myrequest.headRequest(host)
+    # send out request
+    mysocket.send(msg)
+    data = mysocket.receive() # receive a reply from the server
+    print("data received: ", data)
+    
+    mysocket.close()
+    # else:
+    #     print('Please include a valid URL in command line ...')   
     
 
 # call main() method:
---
+++ assignments/asg1/Asg1Urlparser.py
@@ -0,0 +1,53 @@
+# urlparser.py
+class URLparser:
+    
+    def __init__(self):
+        self.port = 80
+
+    def parse(self, string): # string is a url
+        self.query = '' # default query is an empty string
+        self.path = '/' # default path
+        # TODO: Make port dynamic
+        self.port = '80' # default port
+        self.host = '' # host is always defined for valid URLs
+        index = string.find('\n')
+        if index != -1:
+            string = string[:index] # remove line break that is in the end of one line
+        
+        # remove 'http://' or 'https://' if it is present in the URL given
+        index = string.find('://')
+        if index != -1:
+            string = string[index+3:]
+        
+        # remove fragment from url
+        index = string.find('#')
+        if index != -1: # if it found a fragment
+            string = string[:index] # strip fragment
+        
+        # remove user:pass@ if exists
+        index = string.find('@')
+        if index != -1:
+            string = string[index + 1:]
+
+        # get query first, then remove query from url
+        index = string.find('?')
+        if index != -1: # if found a query
+            self.query = string[index:] # get the query
+            string = string[:index] # remove the query from the string
+        
+        # get path next, remove path from url
+        index = string.find('/')
+        if index != -1:
+            self.path = string[index:]
+            string = string[:index]
+
+        # get port
+        index = string.find(':')
+        if index != -1:
+            self.path = string[index + 1:]
+            string = string[:index]
+
+        # get host …
+        self.host = string
+        
+        return self.host, int(self.port), self.path, self.query
